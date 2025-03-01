5ed9bb9fcd71b3a789e72ba936407a6ae71e4e36
Dancan <ngagadancan2003@gmail.com>
2025-03-01 10:05:39 +0300

    Vehicles Endpoints

---
+++ products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="vehicle_images/")
+
+    def __str__(self):
+        return self.name
